4c2cc6b3c28d9aec94f2152c4c719a3fbcd4a3f9
HyunSeong <bolton7425@gmail.com>
2023-06-14 22:55:21 +0900

    props
    - props는 컴포넌트가 받을 수 있는 상태값
    - props는 컴포넌트 내에서 못바꿈, 바꾸고 싶으면 useState를 활용해야함
    - 컴포넌트가 가지고 있는 state를 props로 넘길 수도 있음

--- src/component/Hello.tsx
+++ src/component/Hello.tsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-const Hello = () => {
+const Hello = ({ age }: any) => {
   const [name, setName] = useState("mike");
+  const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
-  function changeName() {
-    const newName = name === "mike" ? "jane" : "mike";
-    setName(newName);
-  }
+  // function changeName() {
+  //   const newName = name === "mike" ? "jane" : "mike";
+  //   setName(newName);
+  // }
   return (
     <>
       <h1>state</h1>
-      <h2>{name}</h2>
-      <button onClick={changeName}>Change</button>
+      <h2>
+        {name} ({age}) : {msg}
+      </h2>
+      <UserName name={name} />
+      <button
+        onClick={() => {
+          setName(name === "mike" ? "jane" : "mike");
+        }}
+      >
+        Change
+      </button>
     </>
   );
 };
