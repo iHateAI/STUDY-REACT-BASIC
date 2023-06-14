1308def3bb122a1255e2a64ad62a831e0e9c3a23
HyunSeong <bolton7425@gmail.com>
2023-06-15 07:55:28 +0900

    Custom hooks
    - 재사용성
    - 그냥 함수라고 생각해도 될 듯

--- src/component/Day.tsx
+++ src/component/Day.tsx
@@ -1,18 +1,10 @@
 import { useParams } from "react-router-dom";
 import Word from "./Word";
-import { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
   const { day } = useParams() as { day: string };
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/words")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setWords(data));
-  }, []);
+  const words = useFetch("http://localhost:3001/words");
 
   const wordList = words.filter((word) => {
     const numDay = parseInt(day, 10);
